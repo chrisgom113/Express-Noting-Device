c039f11134f9e81ef6236bd17cf0444335d58559
chrisgom113 <cgomez4@uci.edu>
2023-03-13 16:41:31 -0700

    added api router and get and post routes

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,2 +1,21 @@
 const api = require('express').Router();
 
+const saveData = require('../db/saveData');
+
+//GET route
+api.get('/notes', (req,res) => {
+    saveData
+    .retrieveNotes()
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json(err));
+});
+
+//POST route
+api.post('/notes', (req, res) => {
+    saveData
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch(err => res.status(500).json(err));
+});
+
+module.exports = api;
