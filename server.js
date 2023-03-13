765d3b4a7a7a07d98ed2393afba34226af0dce73
chrisgom113 <cgomez4@uci.edu>
2023-03-13 16:55:18 -0700

    rearranged folders outside of develop folder

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const api = require('./routes/api');
+const html = require('./routes/html');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use(express.static('public'));
+app.use('/api', api);
+app.use('/', html);
+
+app.listen(PORT, () => {
+    console.log(`Listening now on port ${PORT}!`)
+});
