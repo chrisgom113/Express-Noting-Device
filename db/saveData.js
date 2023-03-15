f0873b90e9b536816234bc5afafc5c3e0cd3d619
chrisgom113 <cgomez4@uci.edu>
2023-03-15 11:54:56 -0700

    added delete method to save class in savadata file

--- db/saveData.js
+++ db/saveData.js
@@ -39,6 +39,12 @@ class Save {
         .then(updatedNotes => this.write(updatedNotes))
         .then(() => newNote);
     }
+
+    deleteNote(id) {
+        return this.retrieveNotes()
+        .then(notes => notes.filter(note => note.id !== id))
+        .then(filteredNotes => this.write(filteredNotes));
+    }
 }
 
 module.exports = new Save();
